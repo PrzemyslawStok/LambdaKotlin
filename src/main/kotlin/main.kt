542e9e508f5880de1b3925a1b8b09d1dc98ecc4a
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-10-24 12:25:05 +0200

    cd..

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,13 @@
 fun main(){
-    println("kotlin")
+    val square = {a:Double->a*a}
+    val add = {a:Int,b:Int->a+b}
+
+    val sub:(Int,Int)->Int = {a:Int,b:Int->a-b}
+
+    println(square(5.0))
+    println(add(2,5))
+}
+
+fun sub1(a:Int,b:Int):Int{
+    return a-b
 }
